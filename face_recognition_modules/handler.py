8a6b51bc4c68670bcbf94f4c14db86f8439e172f
Divya Dharani <divyadoll77@gmail.com>
2022-04-22 21:13:15 -0700

    Checking image recognition module from handler

--- face_recognition_modules/handler.py
+++ face_recognition_modules/handler.py
@@ -1,18 +1,15 @@
 import sys
-import os
-from face_recognition_modules.eval_face_recognition import perform_image_recognition
+from eval_face_recognition import perform_image_recognition
 
 
 def face_recognition_handler(event, context):
     name = 'Alice'
     major = 'Computer Science'
     year = '2021'
-    print(name, major, year)
     print("Event received:", event)
     print("Context received:", context)
     return (name, major, year)
 
 if __name__ == '__main__':
-    print(sys.argv)
-    perform_image_recognition()
-    os.system(f"python eval_face_recognition.py")
+    print("The result is: ", perform_image_recognition())
+    print(face_recognition_handler(None, None))
