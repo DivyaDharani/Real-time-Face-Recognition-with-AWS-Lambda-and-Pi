429efc436482b313b8ef049584314489d8b35f4e
Divya Dharani <divyadoll77@gmail.com>
2022-04-22 21:12:16 -0700

    Commented unused imports; method defined to recognize images

--- face_recognition_modules/eval_face_recognition.py
+++ face_recognition_modules/eval_face_recognition.py
@@ -1,17 +1,44 @@
 import torch
-import torchvision
+#import torchvision
 import torchvision.transforms as transforms
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
-from models.inception_resnet_v1 import InceptionResnetV1
-from urllib.request import urlopen
+#import torch.nn as nn
+#import torch.nn.functional as F
+#import torchvision.models as models
+#from models.inception_resnet_v1 import InceptionResnetV1
+#from urllib.request import urlopen
 from PIL import Image
 import json
 import numpy as np
 import argparse
 import build_custom_model
 
+
+def perform_image_recognition(img_path = "./data/test_me/val/angelina_jolie/1.png"):
+     labels_dir = "./checkpoint/labels.json"
+     model_path = "./checkpoint/model_vggface2_best.pth"
+
+     # read labels
+     with open(labels_dir) as f:
+          labels = json.load(f)
+     print(f"labels: {labels}")
+
+     device = torch.device('cpu')
+     model = build_custom_model.build_model(len(labels)).to(device)
+     model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model'])
+     model.eval()
+     print(f"Best accuracy of the loaded model: {torch.load(model_path, map_location=torch.device('cpu'))['best_acc']}")
+
+     img = Image.open(img_path)
+     img_tensor = transforms.ToTensor()(img).unsqueeze_(0).to(device)
+     outputs = model(img_tensor)
+     _, predicted = torch.max(outputs.data, 1)
+     result = labels[np.array(predicted.cpu())[0]]
+     # print(predicted.data, result)
+
+     img_name = img_path.split("/")[-1]
+     img_and_result = f"({img_name}, {result})"
+     print(f"Image and its recognition result is: {img_and_result}")
+     return img_and_result
 
 if __name__ == "__main__":
 
