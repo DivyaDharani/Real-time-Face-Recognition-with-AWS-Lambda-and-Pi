3fbe3c40f86c3bb32382eaf470ac0cfa642dc3f1
Divya Dharani <divyadoll77@gmail.com>
2022-04-28 23:16:14 -0700

    Print statements added in lambda handler

--- face_recognition_modules/lambda_handler.py
+++ face_recognition_modules/lambda_handler.py
@@ -1,6 +1,12 @@
+import os
 from eval_face_recognition import perform_image_recognition
 
 def face_recognition_handler(event, context):
+    env_var = "AWS_LAMBDA_RUNTIME_API"
+    if env_var in os.environ:
+        print(f"{env_var} = {os.environ[env_var]}")
+    else:
+        print(f"Env var {env_var} not found")
     name = 'Alice'
     major = 'Computer Science'
     year = '2021'
