e520b8c6bfad323b23a99820c5b99d877b6fa12b
Divya Dharani <divyadoll77@gmail.com>
2022-05-04 21:56:49 -0700

    lambda testing

--- http_post_aws_testing.py
+++ http_post_aws_testing.py
@@ -2,7 +2,8 @@ import requests
 with open('my_photo.jpeg', 'rb') as f:
     data = f.read()
 
-url="https://paru1an02g.execute-api.us-east-1.amazonaws.com/prod"    
+#url="https://paru1an02g.execute-api.us-east-1.amazonaws.com/prod"
+url = "https://5zkt8bff7d.execute-api.us-east-1.amazonaws.com/default/cc-project-lambda"
 
 res = requests.post(url,
                     data=data,
